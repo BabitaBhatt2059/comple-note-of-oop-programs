00856a369f3e8c33c9ecd1f600e72de0fa196816
Babita Bhatt <babitabhatta37@gmail.com>
2023-01-25 22:59:01 +0545

    unary operator

---
+++ 11thprogram.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+class space
+{
+    int x,y,z;
+    public:
+    space x(0),y(0),z(0){
+
+    }
+    space (int a,int b,int c);
+    void display();
+    void operator_();
+
+};
+space::display()
+{
+    cout<<"c"<<x<<","<<y<<","<<endl;
+}
+void space::operator_(){
+    x=-x;
+    y=-y;
+    z=-z;
+}
+int main(){
+    space s1,s2(10,-20,30);
+    cout<<"s2=";
+    s2.display();
+    -s2;
+    cout<<"-s2=";
+    s2.display();
+    
+return 0;
+}
