e8a9ff7958a1d25a64fa0a9079d775ceddb127f9
Babita Bhatt <babitabhatta37@gmail.com>
2023-01-26 22:14:55 +0545

    unary operator

--- 11thprogram.cpp
+++ 11thprogram.cpp
@@ -1,33 +1,37 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 class space
 {
-    int x,y,z;
-    public:
-    space x(0),y(0),z(0){
+    int x, y, z;
 
+public:
+    space(){
+        x=0;
+        y=0;
+        z=0;
     }
-    space (int a,int b,int c);
+    space(int a, int b, int c);
     void display();
-    void operator_();
-
+    void operator-();
 };
-space::display()
+void space::display()
 {
-    cout<<"c"<<x<<","<<y<<","<<endl;
+    cout << "c" << x << "," << y << "," << endl;
 }
-void space::operator_(){
-    x=-x;
-    y=-y;
-    z=-z;
+void space::operator-()
+{
+    x = -x;
+    y = -y;
+    z = -z;
 }
-int main(){
-    space s1,s2(10,-20,30);
-    cout<<"s2=";
+int main()
+{
+    space s1, s2(10, -20, 30);
+    cout << "s2=";
     s2.display();
     -s2;
-    cout<<"-s2=";
+    cout << "-s2=";
     s2.display();
-    
-return 0;
+
+    return 0;
 }
