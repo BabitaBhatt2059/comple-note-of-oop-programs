20002fc1486a97924914da58e44009651cb42904
Babita Bhatt <babitabhatta37@gmail.com>
2023-02-01 23:45:49 +0545

    ....

---
+++ 23rdprogram.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class demo {
+    int x,y;
+
+    public:
+    demo(){
+        x=10;
+
+    }
+    demo (demo &z)
+{
+    x=z.x;
+
+}
+void getdata(){
+    cout<<"\n x="<<x;
+
+}
+
+
+};
+int main(){
+    demo aa;
+    demo bb(aa);
+    aa.getdata();
+    
+return 0;
+}
